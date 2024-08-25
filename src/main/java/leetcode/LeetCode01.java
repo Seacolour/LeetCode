a460035794aff9007a12e232753a1d3b4579ae8f
Seacolour <1326192454@qq.com>
2024-08-25 11:36:37 +0800

    8/24

--- src/main/java/leetcode/LeetCode01.java
+++ src/main/java/leetcode/LeetCode01.java
@@ -187,6 +187,66 @@ public class LeetCode01 {
         return count;
     }
 
+    /**
+     * 238. 除自身以外数组的乘积
+     * 输入: nums = [1,2,3,4]
+     * 输出: [24,12,8,6]
+     * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+     * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+     * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     * 用数组保存前后缀，从左到右，从右到左，计算乘积，最后遍历answer数组，赋值即可
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] L = new int[length];
+        int[] R = new int[length];
+        int[] answer = new int[length];
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+        return answer;
+    }
+
+    /**
+     * 125. 验证回文串
+     * 输入: s = "A man, a plan, a canal: Panama"
+     * 输出：true
+     * 解释："amanaplanacanalpanama" 是回文串。
+     * 前后双指针
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        int n = sgood.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            if (Character.toLowerCase(sgood.charAt(left)) != Character.toLowerCase(sgood.charAt(right))) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {7,1,5,3,6,4};
